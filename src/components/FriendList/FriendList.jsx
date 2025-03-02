58117c405e6e6bd7adcb985b7652f99f08d8afbd
Denis Shatalov <denis.shatalov95@gmail.com>
2025-03-02 14:32:32 +0200

    Initial commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,16 @@
+import styles from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem friend={friend} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
