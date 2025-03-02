6771e6ac6b499c6f2eb607068c1db2186ddaf4cc
den4ik <denis.shatalov95@gmail.com>
2025-03-03 01:36:10 +0200

    hw-react-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,47 @@
-import './App.css'
-import Profile from "./Profile/Profile";
-import userData from "../userData.json";
-import friends from "../friends.json";
-import FriendList from "./FriendList/FriendList";
-import transactions from "../transactions.json";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-
-
+import Description from "./Description/Description";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
+import { useState, useEffect } from "react";
+import Notifications from "./Notifications"
 
 export default function App() {
-   return (
-    <div>
-       <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-       />
-         <>
-            <FriendList friends={friends} />
-            <TransactionHistory items={transactions} />
-         </>
-     </div>
-     
+
+    const [clicks, setClicks] = useState(() => {
+        const saveClicks = localStorage.getItem(`selectedCLicks`);
+        if (saveClicks !== null) {
+            return JSON.parse(saveClicks)
+        }
+    })
+
+
+    const handleClick = (key) => {
+    if (key === "reset") {
+        setClicks({ good: 0, neutral: 0, bad: 0 }); 
+    } else {
+        setClicks({
+            ...clicks,
+            [key]: clicks[key] + 1
+        });
+    }
+};
+
+
+    useEffect(() => {
+        localStorage.setItem("selectedCLicks", JSON.stringify (clicks))
+    },[clicks])
     
-  );
+    const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
 
-  
+    return (
+        <div>
 
+    <Description></Description>
+            <Options handleClick={handleClick} totalFeedback={totalFeedback}></Options>
+            { totalFeedback > 0 ? <Feedback clicks={clicks} totalFeedback={totalFeedback}></Feedback> : <Notifications></Notifications> }
+    
+            
+        </div>
+
+    )
+    
 }
-
